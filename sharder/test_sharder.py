e28bca00422a0549f9489a2e37aa34d31ffa5f20
Ian Allison <iana@pims.math.ca>
2023-12-13 11:28:14 -0800

    Explicitly name objects being tested for easier debugging
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- sharder/test_sharder.py
+++ sharder/test_sharder.py
@@ -5,24 +5,27 @@ from tornado import log
 
 from sharder import Sharder
 
+
 @pytest.fixture
 def engine():
-  engine = sqlalchemy.create_engine('sqlite:///:memory:')
-  return engine
+    engine = sqlalchemy.create_engine("sqlite:///:memory:")
+    return engine
+
 
 def test_single_shard(engine):
-    s = Sharder(engine, 'hub', [{'name': 'hub-1'}], log=log.app_log)
-    assert s.shard('user') == 'hub-1'
+    s = Sharder(engine, "hub", [{"name": "hub-1"}], log=log.app_log)
+    assert s.shard("user") == "hub-1"
+
 
 def test_multiple_equal_shards(engine):
     """
     Check that we shard entries equally across buckets. If we have 10 buckets
     and 100 entries, each buckets should see 10 shards
     """
-    buckets = [{'name': str(i)} for i in range(10)]
-    entries = [str(i) for i in range(100)]
+    buckets = [{"name": f"hub-{str(i)}"} for i in range(10)]
+    entries = [f"user-{str(i)}" for i in range(100)]
 
-    s = Sharder(engine, 'hub', buckets, log=log.app_log)
+    s = Sharder(engine, "hub", buckets, log=log.app_log)
     [s.shard(e) for e in entries]
 
     shards = {}
@@ -31,7 +34,7 @@ def test_multiple_equal_shards(engine):
         if shard in shards:
             shards[shard] += 1
         else:
-            shards[shard]  = 1
+            shards[shard] = 1
 
     assert len(shards) == 10
     assert sum(shards.values()) == 100
@@ -39,15 +42,16 @@ def test_multiple_equal_shards(engine):
     for shard, count in shards.items():
         assert count == 10
 
+
 def test_multiple_unequal_shards(engine):
     """
     Check that when the number of entries isn't divisible by the number of
     buckets we still shard as fairly as possible.
     """
-    buckets = [{'name': str(i)} for i in range(10)]
-    entries = [str(i) for i in range(99)]
+    buckets = [{"name": f"hub-{str(i)}"} for i in range(10)]
+    entries = [f"user-{str(i)}" for i in range(99)]
 
-    s = Sharder(engine, 'hub', buckets, log=log.app_log)
+    s = Sharder(engine, "hub", buckets, log=log.app_log)
     [s.shard(e) for e in entries]
 
     shards = {}
@@ -62,16 +66,17 @@ def test_multiple_unequal_shards(engine):
     assert sum(shards.values()) == 99
     assert sorted(shards.values()) == [9, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
-def test_shard_with_offset(engine):
+
+def test_shard_with_extra_shards(engine):
     """
     Check that our sharding policy respects offsets. If a hub has N existing
-    entries in the database and an offset of M, it should behave as is there
-    are N - M users.
+    entries in the database and an extra_shards of M, it should behave as is there
+    are N + M users in that bucket.
     """
-    buckets = [{'name': str(i)} for i in range(10)]
-    entries = [str(i) for i in range(100)]
+    buckets = [{"name": "hub-" + str(i)} for i in range(10)]
+    entries = ["user-" + str(i) for i in range(100)]
 
-    s = Sharder(engine, 'hub', buckets, log=log.app_log)
+    s = Sharder(engine, "hub", buckets, log=log.app_log)
     [s.shard(e) for e in entries]
 
     shards = {}
@@ -80,12 +85,12 @@ def test_shard_with_offset(engine):
         if shard in shards:
             shards[shard] += 1
         else:
-            shards[shard]  = 1
+            shards[shard] = 1
 
     # Add 2 extra_shards to the first bucket.
     # The next 18 assignments should be spread evenly among the remaining hubs
-    s.hubs[0]['extra_shards'] = 2
-    extra_entries = [str(i) for i in range(100,118)]
+    s.hubs[0]["extra_shards"] = 2
+    extra_entries = ["user-" + str(i) for i in range(100, 118)]
 
     for e in extra_entries:
         shard = s.shard(e)
@@ -93,19 +98,20 @@ def test_shard_with_offset(engine):
         if shard in shards:
             shards[shard] += 1
         else:
-            shards[shard]  = 1
+            shards[shard] = 1
 
     assert len(shards) == 10
     assert sum(shards.values()) == 118
     print(shards)
+    print(s)
     for shard, count in shards.items():
-        if shard == buckets[0]['name']:
+        if shard == buckets[0]["name"]:
             assert count == 10
         else:
             assert count == 12
-   
+
     # If we assign 10 more, 1 should go to each bucket
-    extra_entries = [str(i) for i in range(118, 128)]
+    extra_entries = ["user-" + str(i) for i in range(118, 128)]
 
     for e in extra_entries:
         shard = s.shard(e)
@@ -113,13 +119,14 @@ def test_shard_with_offset(engine):
         if shard in shards:
             shards[shard] += 1
         else:
-            shards[shard]  = 1
+            shards[shard] = 1
 
     assert len(shards) == 10
     assert sum(shards.values()) == 128
     print(shards)
+    print(s)
     for shard, count in shards.items():
-        if shard == buckets[0]['name']:
+        if shard == buckets[0]["name"]:
             assert count == 11
         else:
             assert count == 13
